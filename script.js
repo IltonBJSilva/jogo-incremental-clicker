c14a0f3ed684578b6a9e610262d807e374cf838d
Francisco Lopes <ilton.silva@sankhya.com.br>
2018-11-01 16:41:08 -0300

    Layout Modificado

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 var pontuacao = 0;
 var melhoramentos=[false,false,false];
 
-function Rep_Audio(){
+function repeticaoAudio(){
   var som = document.getElementById('meuAudio');
-
-  som.load();
   som.play();
-
 }
 
-
 //Animação no segundo personagem começo
-function Personagem_2(){
-setTimeout(function (){document.getElementById("martelo").src="img/marteloN.png"}, 100);
-
-document.getElementById("martelo").src="img/marteloB.png";
+function personagem2(){
+setTimeout(function (){
+  document.getElementById("martelo").src="img/marteloN.png"}, 100);
+  document.getElementById("martelo").src="img/marteloB.png";
 }
 //Animação no segundo personagem Fim
 
@@ -48,29 +44,40 @@ function tempo(){
 }
 //Fim no cronometro
 
+
 //Função que conta
 function contador(){
-  pontuacao +=1;
+  pontuacao += 1;
   document.getElementById("engragem").innerHTML = "Engrenagem: " + pontuacao;
+  verifica(pontuacao);
+  console.log(pontuacao);
+
 }
+
+function contador2(){
+  pontuacao += 5;
+}
+
 //função que conta fim
 
-//Funçors de melhoramentos acresentando a quantidade pedida
-
-function acresenta_1(){
-    pontuacao +=1;
-    document.getElementById("engragem").innerHTML = "Engrenagem: " + pontuacao;
+function verifica(pontuacao){
+  if(pontuacao == 2 && melhoramentos[0] == false){
+      melhoramentos[0] = true;
+      console.log('up1');
+      setInterval(contador, 20000);
+      ver_1();
+  } else if(pontuacao == 50 && melhoramentos[1] == false){
+      melhoramentos[1] = true;
+      setInterval(contador, 1000);
+      console.log("up2");
+      ver_2();
+  } else if(pontuacao == 70 && melhoramentos[2] == false){
+      melhoramentos[2] = true;
+      setInterval(contador2,10000);
+      console.log("up3");
+      ver_3();
+    }
 }
-
-function acresenta_2(){
-    pontuacao +=4;
-    document.getElementById("engragem").innerHTML = "Engrenagem: " + pontuacao;
-}
-function acresenta_3(){
-    pontuacao +=40;
-    document.getElementById("engragem").innerHTML = "Engrenagem: " + pontuacao;
-}
-//Fim nas funçoes de acressentamento
 
 //Função que aumenta opacity ao comprar o upgrade
 function ver_1(){
@@ -85,41 +92,3 @@ function ver_3(){
   document.getElementById("Mel3").style.opacity = "1.0";
 }
 //fim no aumento de opacity quando o upgrade e comprado
-
-
-//A cada 20 segundo acresenta 1 as Engrenagem
-//Se score for maior ou igual a 60 sera acresentado 4 as Engrenagem
-// e se melhoramentos na possição 0 for igual a false entra na condição
-function Melhoramento_1(){
-    if(pontuacao>=20 && melhoramentos[0]===false){
-        melhoramentos[0]=true;
-        ver_1();
-}
-}
-
-//A cada 60 segundo acresenta 4 as Engrenagem
-//Se score for maior ou igual a 60 sera acresentado 4 as Engrenagem
-// e se melhoramentos na possição 0 for igual a false entra na condição
-function Melhoramento_2(){
-
-  if(pontuacao>=60 && melhoramentos[1]===false){
-      melhoramentos[1]=true;
-      setInterval(acresenta_2, 20000)
-      ver_2();
-}
-
-//A cada 60 segundo acresenta 4 as Engrenagem
-//Se score for maior ou igual a 60 sera acresentado 4 as Engrenagem
-// e se melhoramentos na possição 0 for igual a false entra na condição
-}
-
-function Melhoramento_3(){
-
-  if(pontuacao>=100 && melhoramentos[2]===false){
-      melhoramentos[2]=true;
-      setInterval(acresenta_3, 10000)
-      ver_3();
-    }
-}
-
-//Fim nos melhoramentos
